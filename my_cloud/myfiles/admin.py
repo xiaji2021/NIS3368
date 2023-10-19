04e659244b176a206cc1384c8d64230dee644c6f
neowick <2697809343@qq.com>
2023-10-19 11:24:43 +0800

    后台可upload至./media

--- my_cloud/myfiles/admin.py
+++ my_cloud/myfiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Files
+
+# 注册ArticlePost到admin中
+admin.site.register(Files)
--- my_cloud/myfiles/models.py
+++ my_cloud/myfiles/models.py
@@ -2,23 +2,28 @@ from django.db import models
 from .forms import UploadFileForm
 
 class Files(models.Model):
-    id = models.CharField(max_length=20, default=None, primary_key=True, verbose_name='文件ID')
-    name = models.CharField(max_length=50, default=None, verbose_name='文件名')
-    origin_name = models.CharField(max_length=50, default=None, verbose_name='原始文件名')
-    format = models.CharField(max_length=8, default=None, verbose_name='文件格式')
-    # parent = models.ForeignKey(Catalog, on_delete=models.PROTECT, verbose_name='目录ID')
-    path = models.CharField(max_length=30, default=None, verbose_name='文件路径')
-    size = models.BigIntegerField(default=None, verbose_name='文件大小')
-    md5 = models.CharField(max_length=50, default=None, verbose_name='文件的MD5值')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # upload_to参数，用来指定上传上来的文件保存到哪里
+    myfile = models.FileField(upload_to="media/%Y%m%d/")
+    
+    # id = models.CharField(max_length=20, default=None, primary_key=True, verbose_name='文件ID')
+    # name = models.CharField(max_length=50, default=None, verbose_name='文件名')
+    # origin_name = models.CharField(max_length=50, default=None, verbose_name='原始文件名')
+    # format = models.CharField(max_length=8, default=None, verbose_name='文件格式')
+    # # parent = models.ForeignKey(Catalog, on_delete=models.PROTECT, verbose_name='目录ID')
+    # path = models.CharField(max_length=30, default=None, verbose_name='文件路径')
+    # size = models.BigIntegerField(default=None, verbose_name='文件大小')
+    # md5 = models.CharField(max_length=50, default=None, verbose_name='文件的MD5值')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='Create time')
     update_time = models.DateTimeField(auto_now=True, verbose_name='Update time')
     # objects = models.Manager()
-    file = FileField(max_length=255)
+
 
 
     class Meta:
-        db_table = 'files'
-        indexes = [models.Index(fields=['md5']), models.Index(fields=['create_time']), models.Index(fields=['update_time'])]
+    #     db_table = 'files'
+    #     indexes = [models.Index(fields=['md5']), models.Index(fields=['create_time']), models.Index(fields=['update_time'])]
         ordering = ('-update_time',)
 
     def __str__(self):
