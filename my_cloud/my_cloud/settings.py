1a5e5c2399023953e2445a93c53a46257c92b419
Summer <xiaji2021@sjtu.edu.cn>
2023-11-05 19:47:28 +0800

    更新ssl配置

--- my_cloud/my_cloud/settings.py
+++ my_cloud/my_cloud/settings.py
@@ -29,7 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
